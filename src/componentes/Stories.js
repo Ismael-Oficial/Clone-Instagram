5d356696d7db29bc5fc8020441cb6f189a18b54e
Ismael Chaves Mizael <125465461+Ismael-Oficial@users.noreply.github.com>
2023-04-17 23:02:51 -0300

    Add files via upload

---
+++ src/componentes/Stories.js
@@ -0,0 +1,16 @@
+import styles from './Stories.module.css'
+
+function Stories() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+        </div>
+    )
+}
+
+export default Stories
